0e49a5bd2c56f63f520f93715eba29b85df34d86
Codys80 <131345060+Codys80@users.noreply.github.com>
2026-01-10 20:29:56 +0100

    Dodanie projektu

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace EgzaminProbny6ConsoleApp
+{
+    internal class Program
+    {
+        class Egzamin
+        {
+            public void wyszukaj(int element)
+            {
+                for(int i = 0; i<this.tablica.Length; i++)
+                {
+                    if (this.tablica[i] == element)
+                    {
+                        Console.WriteLine("Odnaleziono " + element + " | Nr indeksu: " + i);
+                        break;
+                    }
+                    else if(i == this.tablica[49])
+                    {
+                        Console.WriteLine("Nie odnaleziono elementu");
+                        break;
+                    }
+                }
+            }
+            private Random losowe = new Random();
+            private int[] tablica = new int[50];
+            public Egzamin()
+            {
+                for(int i = 0; i < this.tablica.Length; i++)
+                {
+                    this.tablica[i] = losowe.Next() % 50;
+                    Console.WriteLine("Nr indeksu: "+ i +" | wartość: "+ this.tablica[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Egzamin E = new Egzamin();
+            E.wyszukaj(20);
+        }
+    }
+}
